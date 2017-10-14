cce371979e5ef6d4ef68f99479b18a15cef2443c
Liana Gareeva <liga.94@bk.ru>
2017-10-14 09:24:03 +0300

    First model "Places" added and checked

--- BestTravelTime/places/models.py
+++ BestTravelTime/places/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=1000)
+
+class Indicators(models.Model):
+    month = models.DateTimeField('tracked month')
+    temperature = models.IntegerField(default=0)
+    precipitation = models.IntegerField(default=0)
+
+class City(models.Model):
+    city_country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    city_name = models.CharField(max_length=1000)
+    indicator = models.ForeignKey(Indicators, on_delete=models.CASCADE)
